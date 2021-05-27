64ac7b37cafb9e9cda3b4d120b6388a6f71cad79
ashmitya <anna.bronytska@gmail.com>
2021-05-27 13:00:12 -0400

    1076 fix

--- 1076_Project_Employees_II.sql
+++ 1076_Project_Employees_II.sql
@@ -1,11 +1,16 @@
 /* Write your PL/SQL query statement below */
-with q as(
-select p.project_id, count(e.employee_id) as emp_amount
-  from project p
-  join employee e
-    on p.employee_id = e.employee_id
- group by p.project_id)
+-- with q as(
+-- select p.project_id, count(p.employee_id) as emp_amount
+--   from project p
+--  group by p.project_id)
 
-select q.project_id
-  from q
- where q.emp_amount = (select max(q.emp_amount) from q)
+-- select q.project_id
+--   from q
+--  where q.emp_amount = (select max(q.emp_amount) from q)
+
+select pp.project_id from(
+select p.project_id,
+       rank() over(order by count(p.employee_id) desc) as rnk
+  from project p
+ group by p.project_id) pp
+ where pp.rnk = 1
