7bd16444a3f167c062d1ae5c8166774c6d5d840a
ashmitya <anna.bronytska@gmail.com>
2021-05-27 13:46:46 -0400

    1211 fix

--- 1211_Queries_Quality_and_Percentage.sql
+++ 1211_Queries_Quality_and_Percentage.sql
@@ -6,10 +6,19 @@ select q.query_name, count(q.query_name) as rat_top
  group by q.query_name
 )
 select q.query_name,
-       round(sum(q.rating/q.position)/count(q.query_name), 2) as quality,
+       round(avg(q.rating/q.position), 2) as quality,
        decode(rt.rat_top, null, 0,
        round(rt.rat_top/count(q.query_name) * 100, 2)) as poor_query_percentage
   from queries q
   left join rt
     on q.query_name = rt.query_name
  group by rt.rat_top, q.query_name
+
+-- select
+--     q.query_name
+--     , round(avg(q.rating/q.position), 2) quality
+--     , round( sum(case when q.rating < 3 then 1 else 0 end) / count(1) * 100, 2) poor_query_percentage
+-- from
+--     queries q
+-- group by
+--     q.query_name
